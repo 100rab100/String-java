8ef4149ca552e6ee8062049d47c2de02343de3f3
100rab100 <99138605+100rab100@users.noreply.github.com>
2022-10-27 06:03:05 +0530

    Update String methods.java

--- String methods.java
+++ String methods.java
@@ -1,25 +1,32 @@
-package stringpractice2;
+package stringpractice1;
 
-public class StringPractice2 {
+public class StringPractice1 {
 
     public static void main(String[] args) {
         
+        String str=new String("netbeans");
+        
         /*
-        String str1="Mr. Shahrukh Khan";
-        System.out.println(str1.startsWith("Mr."));
-        System.out.println(str1.startsWith("Shah" , 4));
-        System.out.println(str1.endsWith("Khan"));
-        System.out.println(str1.charAt(4));
+        //System.out.println(str.length());
+        int len=str.length();
+        System.out.println(len);*/
         
-        for(int i=0;i<str1.length();i++)
-            System.out.print(str1.charAt(i));
-        */
         
-        String str1="www.udemy.co.in";
-        System.out.println(str1.indexOf("."));
-        System.out.println(str1.indexOf(".",4));
-        System.out.println(str1.indexOf("udemy"));
-        System.out.println(str1.lastIndexOf("."));
+        /*String str2=str.toUpperCase();
+        System.out.println(str2);*/
+        
+        
+        /*str=str.trim();
+        System.out.println(str);*/
+        
+        /*String str2=str.substring(3);
+        System.out.println(str2);*/
+        
+        String str2=str.replace('e', 'M');
+        //System.out.println(str2);
+        
+        System.out.println(str2+ " " +str);
+        
     }
     
 }
